6869d1c5c9625aa9731e242a49e31263a78f5c47
Juliya-sun <lyubimova.yulya.29@yandex.ru>
2024-07-18 11:42:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print ("hellow world")
+
+print('1st program')
+print (9**0.5*5)
+print ('2nd program')
+print (9.99>9.98 and 1000!=1000.1)
+print('3rd program')
+print((1234//10-100)+(5678//10-500))
+print('4th program')
+print(((int(13.42) == 42.13*100-4200)) or (int(42.13) == (13.42*100-1300)))
+
